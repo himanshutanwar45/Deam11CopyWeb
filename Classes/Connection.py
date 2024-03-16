c641aae0519ceec07604789b21611177cb4208a7
Himanshu Tanwar <himanshutanwar45@gmail.com>
2024-03-16 21:15:02 +0530

    commit

--- Classes/Connection.py
+++ Classes/Connection.py
@@ -1,7 +1,8 @@
 
 class Connection:
     def GetConnectionString(server_name,database,sql_user,sql_pass):
-        driver = "{SQL Server Native Client 11.0}"
+        #driver = "{SQL Server Native Client 11.0}"
+        driver = "{SQL Server}"
         conn ="Driver="+driver + ";SERVER="+server_name+ ";DATABASE="+database+ ";UID="+sql_user+ ";PWD="+sql_pass
         return conn
     
